f53f129e2858c89298d20d9f7553047d86106afa
papapin777 <papapin777@gmail.com>
2016-10-03 22:06:15 +0300

    inheriting constructor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 class Vehicle {
-
+	constructor(licenceNum){
+		this.licenceNum = licenceNum;
+	}
 }
 
 class Drone extends Vehicle {
@@ -7,9 +9,14 @@ class Drone extends Vehicle {
 }
 
 class Car extends Vehicle {
-
+	constructor(licenceNum){
+		super(licenceNum);
+		
+	}
 }
 
-let car = new Car;
+let car = new Car('123');
 
 console.log(car instanceof Vehicle);
+
+console.log(car.licenceNum);
