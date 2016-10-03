48208c1ad4f5fd8e7f9ab0dac93de2fd89a9de94
papapin777 <papapin777@gmail.com>
2016-10-03 21:59:01 +0300

    extending a class

--- src/app.js
+++ src/app.js
@@ -1,36 +1,15 @@
-class Drone {
-	constructor(id, name){
-		this._id = id;
-		this.name = name;
-	}
-
-	get id( ) {
-		return this._id;
-	}
-
-	set id(value) {
-		this._id = value;
-	}
-
-	static getCompany(){
-		console.log('drone ' + this.id + ' made in ' + this.name);
-	}
-
-	fly() {
-		console.log('drone ' + this.name + ' is flying');
-	}
+class Vehicle {
 
 }
 
-Drone.maxHeight = 2000; //static property
+class Drone extends Vehicle {
 
-let drone = new Drone('123', 'RUBUS');
-drone.id = '1111';
-let drone2 = new Drone('456', 'BRABUS');
+}
 
-drone.fly();
-drone2.fly();
+class Car extends Vehicle {
 
-console.log( drone.id + ' ' + drone.name  );
-console.log( Drone.maxHeight );
-// console.log( drone instanceof Drone );
+}
+
+let car = new Car;
+
+console.log(car instanceof Vehicle);
