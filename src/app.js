3b90e358cf2119fc9595c43bd372e6d9e00846a8
papapin777 <papapin777@gmail.com>
2016-10-04 21:05:09 +0300

    organization code

--- src/app.js
+++ src/app.js
@@ -1,36 +1,8 @@
-class Vehicle {
-	start(){
-		console.log('starting Vehicle' );
-	}
+import {Car} from './classes/car.js';
+import {Drone} from './classes/drone.js';
 
-	static getCompanyName() {
-		console.log('Company NAme' );
-	}
-}
+let car = new Car();
+let drone = new Drone();
 
-class Drone extends Vehicle {
-
-}
-
-class Car extends Vehicle {
-	start(){
-		super.start();
-		console.log('starting Car' );
-	}
-
-
-	static getCompanyName() {
-		super.getCompanyName();
-		console.log('Company Other NAme' );
-	}
-}
-
-let car = new Car('123');
-
-//console.log(car instanceof Vehicle);
-
-car.start();
-
-Car.getCompanyName();
-
-console.log( );
+console.log(car);
+console.log(drone);
